f9509c1ba00d264ba4c0af6198cbec417b7ad810
Mansurbek-Eshbotayev <mustafoimron1820@gmail.com>
2022-06-29 06:01:47 +0500

    Add new change

--- js/main.js
+++ js/main.js
@@ -6,20 +6,31 @@ var elList = document.querySelector(".enjoy__item_one");
 var elListTwo = document.querySelector(".enjoy__item_two");
 var elListThri = document.querySelector(".enjoy__item_thri");
 
+// drink
+var elTitle = document.querySelector(".menu__title_one");
+var elInfo = document.querySelector(".form__inner")
+
+
 elBtn.addEventListener("click", function(){
   elList.classList.add("enjoy__blue");
   elListTwo.classList.remove("enjoy__blue");
   elListThri.classList.remove("enjoy__blue");
-})
+});
 
 elBtnTwo.addEventListener("click", function(){
   elListTwo.classList.add("enjoy__blue");
   elList.classList.remove("enjoy__blue");
   elListThri.classList.remove("enjoy__blue");
-})
+});
 
 elBtnThri.addEventListener("click", function(){
   elListThri.classList.add("enjoy__blue");
   elList.classList.remove("enjoy__blue");
   elListTwo.classList.remove("enjoy__blue");
+});
+
+// drink
+
+elTitle.addEventListener("click" , function(){
+  elInfo.classList.toggle("form__inner_on");
 })
